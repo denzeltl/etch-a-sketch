69621002fd81575acbae9eb46a4832c25b412f4d
Denzel <denzeltlee@gmail.com>
2020-03-07 23:31:13 +0800

    Create css and start with js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const container = document.querySelector(".container");
+
+// functions
+function generateBoxes(size) {
+    const containerWidth = container.offsetWidth;
+    const boxWidth = containerWidth / size;
+    for (let i = size * size; i < array.length; i++) {
+        const element = array[i];
+    }
+}
+
+// events
+window.addEventListener("load", generateBoxes(16));
